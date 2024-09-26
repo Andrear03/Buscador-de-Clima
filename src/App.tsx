6d2bf55e36b9485d91e4d0e0c81d573b849ec9f4
andrear03 <diegoandre0312@gmail.com>
2024-09-26 14:53:49 -0500

    #1 Estructura

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import style from "./App.module.css";
+function App() {
+
+
+  return (
+    <>
+      <h1 className={style.title}>Buscador de Clima</h1>
+      <div className={style.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
+    </>
+  )
+}
+
+export default App
